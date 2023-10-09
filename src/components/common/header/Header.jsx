ce0dc0d8a12e4c2e00d4035c6eabea3c5990c625
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-10 01:05:50 +0900

    Fix: 서브 페이지 라우터 대문자로 시작하는 이슈 수정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -14,17 +14,17 @@ export default function Header() {
       </h1>
       <ul>
         <li>
-          <NavLink to="Posts" activeClassName={"on"}>
+          <NavLink to="posts" activeClassName={"on"}>
             Posts
           </NavLink>
         </li>
         <li>
-          <NavLink to="Review" activeClassName={"on"}>
+          <NavLink to="review" activeClassName={"on"}>
             Review
           </NavLink>
         </li>
         <li>
-          <NavLink to="About" activeClassName={"on"}>
+          <NavLink to="about" activeClassName={"on"}>
             About
           </NavLink>
         </li>
