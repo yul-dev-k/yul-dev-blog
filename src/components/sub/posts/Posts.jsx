ee4cfa1b22f63406a9e1d7755034dfdc61d0255d
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-12 18:39:03 +0900

    Feat: posts.json 화면에 뿌려주도록 코드 수정

--- src/components/sub/posts/Posts.jsx
+++ src/components/sub/posts/Posts.jsx
@@ -1,6 +1,24 @@
 import Layout from "../../common/layout/Layout";
 import "./Posts.scss";
+import postList from "../../../posts.json";
+
+function Post({ post }) {
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <span>{post.date}</span>
+      <div>{post.tags}</div>
+      <div>{post.content}</div>
+    </>
+  );
+}
 
 export default function Posts() {
-  return <Layout title={"Posts"}>posts children</Layout>;
+  return (
+    <Layout title={"Posts"}>
+      {postList.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </Layout>
+  );
 }
