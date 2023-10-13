281cf7e46f38b819817c4339b4b98aeac6263b27
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-14 00:28:37 +0900

    Feat: remarkGfm패키지 통해 다양한 마크다운 지원

--- src/components/sub/posts/Posts.jsx
+++ src/components/sub/posts/Posts.jsx
@@ -2,15 +2,38 @@ import Layout from "../../common/layout/Layout";
 import "./Posts.scss";
 import postList from "../../../posts.json";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 function Post({ post }) {
+  const markdown = post.content;
+
   return (
-    <>
+    <div className="md-content">
       <h1>{post.title}</h1>
       <span>{post.date}</span>
       <div>{post.tags}</div>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
-    </>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        children={markdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                language={match[1]}
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      ></ReactMarkdown>
+    </div>
   );
 }
 
--- src/components/sub/posts/content/firtstPost.md
+++ src/components/sub/posts/content/firtstPost.md
@@ -8,3 +8,23 @@ serises: test
 # Welcome to My Blog
 
 post1
+
+```javascript
+// function that adds "2 numbers" together
+const sumTwoNumbers = (num1, num2) => num1 + num2;
+
+// call the function
+console.log(sumTwoNumbers(1, 2)); // 3
+
+// array of users
+const users = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 28 },
+  { name: "Bob", age: 25 },
+];
+
+// print out the users age
+console.log(users.map((user) => user.age)); // [30, 28, 25]
+```
+
+> test
