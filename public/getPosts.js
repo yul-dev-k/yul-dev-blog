b182762c7b72210aef32b5639267dcc0634453a7
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-20 23:15:17 +0900

    Fix: fs.readFile 비동기 문제로 md 파일 읽기가 안되던 이슈 수정

--- public/getPosts.js
+++ public/getPosts.js
@@ -4,61 +4,68 @@ const fs = require("fs");
 const dirPath = path.join(__dirname, "../src/components/sub/posts/content");
 let postList = [];
 
-const getPosts = () => {
-  fs.readdir(dirPath, (err, files) => {
-    if (err) {
-      return console.log("failed to list contents of directory: " + err);
-    }
-    files.forEach((file, i) => {
+const getPosts = async () => {
+  try {
+    const files = await fs.promises.readdir(dirPath);
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
       let obj = {};
-      let post;
-      fs.readFile(`${dirPath}/${file}`, "utf8", (err, contents) => {
-        const getMeatadataIndices = (acc, elem, i) => {
-          if (/^---/.test(elem)) {
-            acc.push(i);
-          }
-          return acc;
-        };
-        const parseMetadata = ({ lines, metaDataIndices }) => {
-          if (metaDataIndices.length > 0) {
-            let metadata = lines.slice(
-              metaDataIndices[0] + 1,
-              metaDataIndices[1]
-            );
-            metadata.forEach((line) => {
-              obj[line.split(": ")[0]] = line.split(": ")[1];
-            });
-            console.log(obj);
-            return obj;
-          }
-        };
-        const parseContet = ({ lines, metaDataIndices }) => {
-          if (metaDataIndices.length > 0) {
-            lines = lines.slice(metaDataIndices[1] + 1, lines.length);
-          }
-          return lines.join("\n");
-        };
-        const lines = contents.split("\n");
-        const metaDataIndices = lines.reduce(getMeatadataIndices, []);
-        const metadata = parseMetadata({ lines, metaDataIndices });
-        const content = parseContet({ lines, metaDataIndices });
-        post = {
-          id: i + 1,
-          title: metadata.title ? metadata.title : "No title given",
-          date: metadata.date ? metadata.date : "No date given",
-          tags: metadata.tags ? metadata.tags : "No tags given",
-          serises: metadata.serises ? metadata.serises : "No serises given",
-          summery: metadata.summery ? metadata.summery : "No summery given",
-          content: content ? content : "No content given",
-        };
-        postList.push(post);
-        if (i === files.length - 1) {
-          let data = JSON.stringify(postList);
-          fs.writeFileSync("src/posts.json", data);
-        }
-      });
-    });
-  });
+
+      const contents = await fs.promises.readFile(
+        path.join(dirPath, file),
+        "utf8"
+      );
+      const lines = contents.split("\n");
+
+      const metaDataIndices = lines.reduce(getMetadataIndices, []);
+      const metadata = parseMetadata({ lines, metaDataIndices });
+      const content = parseContent({ lines, metaDataIndices });
+
+      const post = {
+        id: i + 1,
+        title: metadata.title ? metadata.title : "No title given",
+        date: metadata.date ? metadata.date : "No date given",
+        tags: metadata.tags ? metadata.tags : "No tags given",
+        serises: metadata.serises ? metadata.serises : "No series given",
+        summary: metadata.summary ? metadata.summary : "No summary given",
+        content: content ? content : "No content given",
+      };
+      postList.push(post);
+
+      if (i === files.length - 1) {
+        let data = JSON.stringify(postList);
+        fs.writeFileSync("src/posts.json", data);
+      }
+    }
+  } catch (err) {
+    console.error("Error:", err);
+  }
 };
 
 getPosts();
+
+function getMetadataIndices(acc, elem, i) {
+  if (/^---/.test(elem)) {
+    acc.push(i);
+  }
+  return acc;
+}
+
+function parseMetadata({ lines, metaDataIndices }) {
+  let obj = {}; // obj 선언
+  if (metaDataIndices.length > 0) {
+    let metadata = lines.slice(metaDataIndices[0] + 1, metaDataIndices[1]);
+    metadata.forEach((line) => {
+      obj[line.split(": ")[0]] = line.split(": ")[1].trim(); // Trim whitespace
+    });
+    return obj;
+  }
+  return {};
+}
+
+function parseContent({ lines, metaDataIndices }) {
+  if (metaDataIndices.length > 0) {
+    lines = lines.slice(metaDataIndices[1] + 1, lines.length);
+  }
+  return lines.join("\n");
+}
