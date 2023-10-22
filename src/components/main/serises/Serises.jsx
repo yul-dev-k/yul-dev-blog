a7e0c9e1e21c663ebed2d8414029cabf2eeb96aa
kimgyul <142677224+yul-dev-k@users.noreply.github.com>
2023-10-22 09:58:45 +0900

    Feat: serises 메타 태그 중 중복되고 null이 아닌 값을 받아 컴포넌트 생성

--- src/components/main/serises/Serises.jsx
+++ src/components/main/serises/Serises.jsx
@@ -1,5 +1,17 @@
 import "./Serises.scss";
+import postList from "../../../posts.json";
 
 export default function Serises() {
-  return <div className="serises">Serises</div>;
+  const distinctSerises = postList
+    .map((post) => post.serises)
+    .filter((el, indx, arr) => el && arr.indexOf(el) === indx)
+    .filter((el) => !(el === "null"));
+
+  return (
+    <ul className="serises">
+      {distinctSerises.map((el, idx) => (
+        <li key={idx}>{el}</li>
+      ))}
+    </ul>
+  );
 }
